e5b1412fe8a8ad8f4c86b836000b25fc072a4e5c
Arvid Lindenau <arvid.lindenau@nerdware.dev>
2023-12-30 17:33:00 +0100

    simplify migrations

--- priv/repo/migrations/20231230150701_add_username_to_user.exs
+++
@@ -1,9 +0,0 @@
-defmodule EcclesiaDb.Repo.Migrations.AddUsernameToUser do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :username, :citext, null: false
-    end
-  end
-end
